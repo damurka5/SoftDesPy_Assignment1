f44d86f148699d22e60a195d485f42c9c3bb04b1
Damir Nurtdinov <damurka777@gmail.com>
2024-09-26 10:10:19 +0300

    statistics added, task checker added

--- task_checker.py
+++ task_checker.py
@@ -1,3 +1,29 @@
+user_stats = {} # dict with statistics about solved tasks, number of attempts, errors
+#  {
+#     user_name: {
+#         tasks: [task_id],
+#         attempts_for_each_task : {task_id: int}
+#     }
+# }
+
+def counter(func):
+    def wrapper(*args, **kwargs):
+        user_name = kwargs['user_name']
+        if user_name in user_stats:
+            user_stats[user_name]['tasks'].append(kwargs['task_id'])
+            user_stats[user_name]['attempts_for_each_task'][kwargs['task_id']] += 1
+        else:
+            user_stats[user_name] = {
+                'tasks':[kwargs['task_id']],
+                'attempts_for_each_task':{
+                    kwargs['task_id']:1
+                }
+            }
+        return func(*args, **kwargs)
+    return wrapper
+        
+        
+
 class Test:
     def __init__(self, io_file):
         self.file = io_file
@@ -19,11 +45,20 @@ class Test:
 class Task:
     def __init__(self, file_name, tests):
         self.description = self.read_file(file_name)
-        self.tests = Test(file_name) # [Test]
+        self.tests = Test(file_name) 
         
     def read_file(self, file_name):
         with open(file=f'hw1/tasks/description/{file_name}') as f:
             return f.read()
+        
+class TaskChecker:
+    def __init__(self, file_name):
+        self.task = Task(file_name=file_name)
+        self.test = Test(io_file=file_name)
+        
+    @counter    
+    def run_tests(self, user_name='None', task_id=-1):
+        ... # TODO code with exec
     
 if __name__ == '__main__':
     print('Hey')
