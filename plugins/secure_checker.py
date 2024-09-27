717be93e31613716ab19355661f37f614c1af78a
Damir Nurtdinov <damurka777@gmail.com>
2024-09-27 09:34:10 +0300

    lambda function added

--- plugins/secure_checker.py
+++ plugins/secure_checker.py
@@ -3,8 +3,8 @@ from plugins_interface import PluginInterface
 class SecurityChecker(PluginInterface):
     def run(self, code):
         malicious_words = ['import os']
-        for imp in malicious_words:
-            if imp in code:
-                print(f'Обнаружено небезопасное выражение: {imp}')
-                return False
+        is_safe = lambda code: not any(imp in code for imp in malicious_words)
+        if not is_safe(code):
+            print(f'Обнаружено небезопасное выражение в коде!')
+            return False
         return True
